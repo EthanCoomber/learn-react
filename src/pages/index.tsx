22c9e54ac578a238b86f0bd120c74c002a4140ab
Ecoomber327 <78524305+Ecoomber327@users.noreply.github.com>
2025-01-21 19:13:25 -0500

    4 files updated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,24 @@
 // import Image from 'next/image'
 // import { Inter } from 'next/font/google'
 // import styles from '@/styles/Home.module.css'
-import Profile from './components/profile'
+// import ToDos from './components/todos'
+import Recipes from './qcomps/recipes'
+import GalleryProps from './qcomps/gallery_props'
+import PackingList from './qcomps/props_item'
+import List from './qcomps/list_keys_id'
 
 export default function Home() {
   return (
-    <Profile />
+    <div>
+      <h1>Home</h1>
+      <h2>Recipes</h2>
+      <Recipes />
+      <h2>GalleryProps</h2>
+      <GalleryProps />
+      <h2>PackingList</h2>
+      <PackingList />
+      <h2>List</h2>
+      <List />
+    </div>
   )
 }
